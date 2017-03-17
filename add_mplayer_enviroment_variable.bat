37cc77a922c1218422bbb04d9bbc1808f6120886
EinsteinXXL <EinsteinXXL@gmx.net>
2017-03-17 04:10:08 +0100

    fixed bug in enviroment adding

--- add_mplayer_enviroment_variable.bat
+++ add_mplayer_enviroment_variable.bat
@@ -3,6 +3,6 @@
 Echo.%PATH% | findstr /I /C:"\\MPlayer">nul && (
     echo.Enviroment variable for MPlayer was already set!
 ) || (
-    setenv.exe -u PATH "%CD%\resources\app\media\mplayer"
-    echo.Enviroment variable for MPlayer successfully set! > enviroment_done 
+    setenv.exe -u PATH "%PATH%;%CD%\resources\app\media\mplayer"
+    echo.Enviroment variable for MPlayer successfully set!
 )
